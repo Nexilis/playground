40ca0e74ade2fcae6ac37ba423d97ba6bf7572f9
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-09-19 10:18:44 +0200

    Sphere's construction func

--- fennel/raymarching/main.fnl
+++ fennel/raymarching/main.fnl
@@ -6,5 +6,19 @@
 
 (var camera-x-rotation (.. "Camera x rotation is: " (tostring camera.x-rotate)))
 
+;; todo: impl sphere-distance
+
+(fn sphere [radius pos color]
+    (let [[x y z] (or pos [0 0 0])
+          [r g b] (or color [1 1 1])]
+        {:radius (or radius 5)
+         :pos [(or x 0) (or y 0) (or z 0)]
+         :color [(or r 0) (or g 0) (or b 0)]
+         :sdf sphere-distance
+         ;; sdf explained here https://en.wikipedia.org/wiki/Signed%5Fdistance%5Ffunction
+         }
+    )
+)
+
 (fn love.draw []
     (love.graphics.print camera-x-rotation))
--- fennel/raymarching/main.lua
+++ fennel/raymarching/main.lua
@@ -1,5 +1,17 @@
 local camera = {["x-rotate"] = 0.0, ["z-rotate"] = 0.0, pos = {0.0, 0.0, 0.0}}
+local scene = {}
 local camera_x_rotation = ("Camera x rotation is: " .. tostring(camera["x-rotate"]))
+local function sphere(radius, pos, color)
+  local _let_1_ = (pos or {0, 0, 0})
+  local x = _let_1_[1]
+  local y = _let_1_[2]
+  local z = _let_1_[3]
+  local _let_2_ = (color or {1, 1, 1})
+  local r = _let_2_[1]
+  local g = _let_2_[2]
+  local b = _let_2_[3]
+  return {color = {(r or 0), (g or 0), (b or 0)}, pos = {(x or 0), (y or 0), (z or 0)}, radius = (radius or 5), sdf = __fnl_global__sphere_2ddistance}
+end
 love.draw = function()
   return love.graphics.print(camera_x_rotation)
 end
