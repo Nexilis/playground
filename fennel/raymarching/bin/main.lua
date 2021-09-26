1006a39c17eb6406636c25225b18d73797ceeb74
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-09-26 08:42:40 +0200

    Remove compilation results

--- fennel/raymarching/bin/main.lua
+++
@@ -1,7 +0,0 @@
-local logic = require("logic")
-love.draw = function()
-  love.graphics.print(logic["sphere-distance"](logic.sphere(5), {5, 0, 0}), 0, 0)
-  love.graphics.print(logic["sphere-distance"](logic.sphere(5), {0, 15, 0}), 0, 15)
-  return love.graphics.print(logic["sphere-distance"](logic.sphere(5), {0, 0, 0}), 0, 30)
-end
-return love.draw
--- fennel/raymarching/tests/tests.lua
+++
@@ -1,7 +0,0 @@
-local lu = require("luaunit")
-local sut = require("logic")
-TestSuite = {}
-TestSuite["test-true-is-true"] = function()
-  return lu.assertEquals(true, true)
-end
-return os.exit(lu.LuaUnit.run())
